a4d0925d76a78ea1b1947de932902a300409dc74
Devkamal Gogoi <devkamalofficial99@gmail.com>
2023-02-23 14:06:59 +0530

    008

---
+++ Convert_Binary_Number_in_a_Linked_List_to_Integer/Solution.java
@@ -0,0 +1,12 @@
+package Convert_Binary_Number_in_a_Linked_List_to_Integer;
+
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = num * 2 + head.next.val;
+            head = head.next;    
+        }
+        return num;
+    }
+}
