a3bb613d1ace79e4b4f99cf47f1d40ad6daa4f97
Devkamal Gogoi <devkamalofficial99@gmail.com>
2023-02-23 14:21:15 +0530

    009

---
+++ Middle_of_the_Linked_List/Middle_of_the_Linked_List.java
@@ -0,0 +1,13 @@
+package Middle_of_the_Linked_List;
+
+public class Middle_of_the_Linked_List {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+    
